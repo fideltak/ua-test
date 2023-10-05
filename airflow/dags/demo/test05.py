f4ae7fb0acd4aed7af0e0792f85eccf6a1954594
Tak <d.a.takuzone@gmail.com>
2023-10-05 12:29:11 +0900

    add new dag

---
+++ airflow/dags/demo/test05.py
@@ -0,0 +1,29 @@
+import airflow
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from pip import _internal
+
+args = {
+    'owner': 'tak',
+}
+
+dag = DAG(
+    dag_id='pip',
+    default_args=args,
+    start_date=airflow.utils.dates.days_ago(2),
+    schedule_interval=None,
+)
+
+def get_pip_list():
+    p_list = _internal.main(['list'])
+    print(p_list)
+    return p_list
+
+run_this = PythonOperator(
+    task_id='execute_pip_list',
+    provide_context=True,
+    python_callable=get_pip_list,
+    dag=dag,
+)
+
+run_this
