5cf0e71a6412005fa2772afcb71881164fbc205f
Tak <d.a.takuzone@gmail.com>
2023-10-26 10:09:51 +0900

    new dag

--- airflow/dags/dag_test.py
+++ airflow/dags/dag_test.py
@@ -3,60 +3,179 @@ from os.path import exists
 
 # Airflow Variable Check
 ## Use same Variables as Dag Codes deliberately
-test_flag = Variable.get("dag_sample_test_enabled",default_var="true")
-file_path = Variable.get("file_path",default_var="/tmp/test.txt")
+test_flag = Variable.get("dag_sample_test_enabled",
+                         default_var="true")  # Test Flag
+file_path = Variable.get("file_path", default_var="/tmp/test.txt")
 if test_flag.lower() in ['true', '1', 'yes', 'maybe', 'yeah', 'yup']:
     test_flag = True
 else:
     test_flag = False
 
+
 # Test Codes
-def dag_test(task_name):
+def unit_test(name):
     if test_flag:
-        if task_name == "task01":
+        if name == "create_file":
+
             def _test(task):
-                def task01(*args): # this method name should be same as task name
+
+                def create_file(
+                        *args):  # this method name should be same as task name
                     task(*args)
-                    print("##########################TEST: {}#################################".format(task_name))
-                    print("Test Case01: Check file existance: {}".format(file_path))
+                    print(
+                        "########################## UNIT TEST: {} #################################"
+                        .format(name))
+                    print("Test Case01: Check file existance: {}".format(
+                        file_path))
                     if _check_file_existance(file_path):
                         print("Test Case01: Passed")
                     else:
                         print("Failed")
-                        raise ValueError ("No file: {}".format(file_path))
-                    print("########################################################################")
-                return task01
+                        raise ValueError("No file: {}".format(file_path))
+                    print(
+                        "########################################################################"
+                    )
+
+                return create_file
+
             return _test
-        if task_name == "task02": # this method name should be same as task name
+        if name == "create_file_and_remove":  # this method name should be same as task name
+
             def _test(task):
-                def task02(*args):
+
+                def create_file_and_remove(*args):
                     task(*args)
-                    print("##########################TEST: {}#################################".format(task_name))
-                    print("Test Case01: Check file NOT existance: {}".format(file_path))
+                    print(
+                        "########################## UNIT TEST: {} #################################"
+                        .format(name))
+                    print("Test Case01: Check file NOT existance: {}".format(
+                        file_path))
                     if not _check_file_existance(file_path):
                         print("Test Case01: Passed")
                     else:
                         print("Failed")
-                        raise ValueError ("No file: {}".format(file_path))
-                    print("########################################################################")
-                return task02
+                        raise ValueError("No file: {}".format(file_path))
+                    print(
+                        "########################################################################"
+                    )
+
+                return create_file_and_remove
+
             return _test
-        else: 
+        else:
+
             def _test(task):
+
                 def others(*args):
                     task(*args)
-                    print("##########################TEST: {}#################################".format(task_name))
+                    print(
+                        "########################## UNIT TEST: {} #################################"
+                        .format(name))
                     print("Skipped...")
-                    print("Not defined test: {}".format(task_name))
-                    print("########################################################################")
+                    print("Not defined test: {}".format(name))
+                    print(
+                        "########################################################################"
+                    )
+
                 return others
+
             return _test
     else:
+
         def _no_test(task):
+
             def _wrapper(*args):
+                print(
+                    "########################## TEST Skipped: {} #################################"
+                    .format(name))
                 task(*args)
+
             return _wrapper
+
         return _no_test
+
+
+def task_test(name):
+    if test_flag:
+        if name == "task01":
+
+            def _test(task):
+
+                def task01(
+                        *args):  # this method name should be same as task name
+                    task(*args)
+                    print(
+                        "########################## Task TEST: {} #################################"
+                        .format(name))
+                    print("Test Case01: Check file existance: {}".format(
+                        file_path))
+                    if _check_file_existance(file_path):
+                        print("Test Case01: Passed")
+                    else:
+                        print("Failed")
+                        raise ValueError("No file: {}".format(file_path))
+                    print(
+                        "########################################################################"
+                    )
+
+                return task01
+
+            return _test
+        if name == "task02":  # this method name should be same as task name
+
+            def _test(task):
+
+                def task02(*args):
+                    task(*args)
+                    print(
+                        "########################## Task TEST: {} #################################"
+                        .format(name))
+                    print("Test Case01: Check file NOT existance: {}".format(
+                        file_path))
+                    if not _check_file_existance(file_path):
+                        print("Test Case01: Passed")
+                    else:
+                        print("Failed")
+                        raise ValueError("No file: {}".format(file_path))
+                    print(
+                        "########################################################################"
+                    )
+
+                return task02
+
+            return _test
+        else:
+
+            def _test(task):
+
+                def others(*args):
+                    task(*args)
+                    print(
+                        "########################## Task TEST: {} #################################"
+                        .format(name))
+                    print("Skipped...")
+                    print("Not defined test: {}".format(name))
+                    print(
+                        "########################################################################"
+                    )
+
+                return others
+
+            return _test
+    else:
+
+        def _no_test(task):
+
+            def _wrapper(*args):
+                print(
+                    "########################## TEST Skipped: {} #################################"
+                    .format(name))
+                task(*args)
+
+            return _wrapper
+
+        return _no_test
+
 
 # Test Methods
 def _check_file_existance(file_path):
--- airflow/dags/demo/dag.py
+++ airflow/dags/demo/dag.py
@@ -1,15 +1,15 @@
 import airflow
 from airflow.decorators import dag, task
 from airflow.models import Variable
-from dag_test import dag_test
+from dag_test import unit_test, task_test
 import os
 
-file_path = Variable.get("file_path",default_var="/tmp/test.txt")
-
+file_path = Variable.get("file_path", default_var="/tmp/test.txt")
 
 args = {
     'owner': 'tak',
 }
+
 
 @dag(
     dag_id='dag_sample',
@@ -17,31 +17,55 @@ args = {
     start_date=airflow.utils.dates.days_ago(2),
     schedule=None,
 )
-
-
 def dag_sample():
-    @task
-    @dag_test('task01')
-    def task01():
+
+    # Dag Task Methods
+    @unit_test('create_file')  # Test for method
+    def create_file():
         print("Dag Task: Creating a file")
         with open(file_path, 'w') as f:
             f.write('Hello!')
         print("Created a file: {}".format(file_path))
 
-    @task
-    @dag_test('task02')
-    def task02():
-        print("Dag Task: Creating file and Remove it")
-        with open(file_path, 'w') as f:
-            f.write('Hello!')
+    @unit_test('create_file_and_remove')  # Test for method
+    def create_file_and_remove():
+        create_file()
         os.remove(file_path)
-        
-    @task
-    @dag_test('dummy')
-    def dummy():
+
+    @unit_test('dummy01')  # Test for method
+    def dummy01():
         print("Dag Task: Dummy task")
         print("This is dummy")
 
-    task01()>>task02()>>dummy()
+    # Dag Tasks
+    @task
+    def task01():
+
+        @task_test('task01')  # Test for Dag Task
+        def _do():
+            create_file()
+
+        _do()
+
+    @task
+    def task02():
+
+        @task_test('task02')  # Test for Dag Task
+        def _do():
+            create_file_and_remove()
+
+        _do()
+
+    @task
+    def dummy01_task():
+
+        @task_test('dummy01_task')  # Test for Dag Task
+        def _do():
+            dummy01()
+
+        _do()
+
+    task01() >> task02() >> dummy01_task()
+
 
 dag_sample()
